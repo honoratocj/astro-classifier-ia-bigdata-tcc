c319677abc2fc238ddadfdfe51fbf416c90edba0
honoratocj <148789840+honoratocj@users.noreply.github.com>
2025-08-05 14:58:52 -0300

    First Upload and Commit

---
+++ Querys Used on SDSS to Extract Data/PhotoObjAll_SpecObjAll_galSpecInfo_galSpecLine_Pt1.sql
@@ -0,0 +1,52 @@
+SELECT TOP 500000
+    p.specObjID as specObjID_p, 
+    p.skyVersion, 
+    p.run, 
+    p.rerun, 
+    p.camcol, 
+    p.field, 
+    p.obj, 
+    p.psfMag_u, 
+    p.psfMag_g, 
+    p.psfMag_r, 
+    p.psfMag_i, 
+    p.psfMag_z, 
+    p.fiberMag_u, 
+    p.fiberMag_g, 
+    p.fiberMag_r, 
+    p.fiberMag_i, 
+    p.fiberMag_z, 
+    p.petroMag_u, 
+    p.petroMag_g, 
+    p.petroMag_r, 
+    p.petroMag_i, 
+    p.petroMag_z, 
+    p.petroFlux_u, 
+    p.petroFlux_g, 
+    p.petroFlux_r, 
+    p.petroFlux_i, 
+    p.petroFlux_z, 
+    s.z, 
+    s.subclass, 
+    s.class, 
+    g.specObjID as specObjID_g, 
+    g.spectrotype, 
+    g.z as redshift, 
+    gs.oii_3726_flux, 
+    gs.oiii_5007_flux, 
+    gs.h_alpha_flux, 
+    gs.oii_3726_chisq, 
+    gs.oiii_5007_chisq, 
+    gs.h_alpha_chisq, 
+    gs.oii_3726_inst_res, 
+    gs.oiii_5007_inst_res, 
+    gs.h_alpha_inst_res
+FROM 
+    PhotoObjAll p
+JOIN 
+    SpecObjAll s ON p.specObjID = s.specObjID
+LEFT JOIN 
+    galSpecInfo g ON p.specObjID = g.specObjID
+LEFT JOIN 
+    galSpecLine gs ON p.specObjID = gs.specObjID
+WHERE p.ObjID < 1237663000000000000 AND g.specObjID IS NOT NULL
---
+++ Querys Used on SDSS to Extract Data/PhotoObj_SpecObj_Pt2.sql
@@ -0,0 +1,8 @@
+SELECT TOP 500000
+    p.objid, p.ra, p.dec, p.u, p.g, p.r, p.i, p.z,
+    p.run, p.rerun, p.camcol, p.field,
+    s.specobjid, s.class, s.z AS redshift,
+    s.plate, s.mjd, s.fiberid
+FROM PhotoObj AS p
+JOIN SpecObj AS s ON s.bestobjid = p.objid
+WHERE p.objid  < 1237670000000000000
